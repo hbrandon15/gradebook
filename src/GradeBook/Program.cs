5737a37d1b057a1834da89ae6ad85f773dbcf592
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-04-13 09:49:38 -0500

    Adding a loop for array
    Added loop for the array to iterate through and calculate the sum of all elements.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,14 +6,15 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var numbers = new double[3]; 
-      numbers[0] = 12.7; 
-      numbers[1] = 13.5;
-      numbers[2] = 20.1;
+      var numbers = new[] { 12.7, 13.5, 20.1 };
 
-      var sum = numbers[0] + numbers[1] + numbers[2];
+      var sum = 0.0;
+      foreach (double number in numbers)
+      {
+        sum += number;
+      }
       System.Console.WriteLine(sum);
-      
+
       if (args.Length > 0)
       {
         Console.WriteLine($"Hello, {args[0]}!");
