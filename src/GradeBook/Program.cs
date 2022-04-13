9ba559fdf4a5944794a17a2d9d340eb24d0a3e0c
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-04-13 09:32:55 -0500

    Creating and initializing array
    Created a new array and filled it with doubles. Then I calculate the sum of that array.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 
-namespace GradeBook // Note: actual namespace depends on the project name.
+namespace GradeBook
 {
   internal class Program
   {
     static void Main(string[] args)
     {
+      var numbers = new double[3]; 
+      numbers[0] = 12.7; 
+      numbers[1] = 13.5;
+      numbers[2] = 20.1;
+
+      var sum = numbers[0] + numbers[1] + numbers[2];
+      System.Console.WriteLine(sum);
+      
       if (args.Length > 0)
       {
         Console.WriteLine($"Hello, {args[0]}!");
