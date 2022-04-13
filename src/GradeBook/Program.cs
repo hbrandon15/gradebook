59777c99125578e748d48bb5bc4bc9277357988c
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-04-13 10:32:04 -0500

    Formatting results
    Formatted our sum and average to insure we are only getting 1 decimal place after the decimal.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,6 @@ namespace GradeBook
   {
     static void Main(string[] args)
     {
-      var numbers = new[] { 12.7, 13.5, 20.1 };
       var grades = new List<double>() { 12.7, 13.5, 20.1 };
       grades.Add(56.1);
 
@@ -17,8 +16,8 @@ namespace GradeBook
         sum += number;
       }
       var avg = sum/grades.Count;
-      System.Console.WriteLine($"The sum is: {sum}");
-      System.Console.WriteLine($"The average is: {avg}");
+      System.Console.WriteLine($"The sum is: {sum:N1}");
+      System.Console.WriteLine($"The average is: {avg:N1}");
 
       if (args.Length > 0)
       {
