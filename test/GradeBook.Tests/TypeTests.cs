32f39ddda8ee6ac076de0dafff1b8732c3eb3674
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-04-19 08:30:24 -0500

    Unit Test: Passing values
    Unit testing: passing parameters by value

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -9,29 +9,47 @@ public class TypeTests
   public void Test1()
   {
 
-    // arrange section 
-    // this is where you put all your test data and arrange
-    // object and values
+    var book1 = GetBook("Book 1");
+    SetName(book1, "New Name");
+
+    Assert.Equal("Book 1", book1.Name);
+  }
+
+
+  [Fact]
+  public void GetBookReturnsDifferentObjects()
+  {
+
     var book1 = GetBook("Book 1");
     var book2 = GetBook("Book 2");
 
-    Book GetBook(string name)
-    {
-      return new Book(name);
-    }
-
-    // act section
-    // this is where you invoke a method to perform a computation
-
-
-    // assert section
-    // this is where you assert something of the value 
-    // that is calculated in the act section
     Assert.Equal("Book 1", book1.Name);
     Assert.Equal("Book 2", book2.Name);
-
-
+    Assert.NotSame(book1, book2);
   }
+
+
+  [Fact]
+  public void TwoVarsCanReferenceSameObject()
+  {
+
+    var book1 = GetBook("Book 1");
+    var book2 = book1;
+
+    Assert.Same(book1, book2);
+    Assert.True(Object.ReferenceEquals(book1, book2));
+  }
+
+  Book GetBook(string name)
+  {
+    return new Book(name);
+  }
+
+  private void SetName(Book book, string name)
+  {
+    book.Name = name;
+  }
+
 
 
 }
